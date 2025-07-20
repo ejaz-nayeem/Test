9e926795d0074ea0483aff21e2a859c521c3c6e9
ejaz-nayeem <muhammadejazbinnayeem@gmail.com>
2025-07-20 19:37:39 +0600

    update

--- telusko/settings.py
+++ telusko/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aip8(%%qgy(wcp730ypkeac2hu*8u@wcafe=zg6-q+z6mk=ctn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calc'
+    'calc',
+    'travello',
     
 ]
 
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
---
+++ travello/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Destination:
+    id:int
+    destination_name:str
+    
+    description:str
+    price:int
+    image:str
+    offer:bool
+
+# class Destination2:
+#     id:int
+#     destination_name:str
+    
+#     description:str
+#     price:int
+#     image:str
+    
+# class Destination3:
+#     id:int
+#     destination_name:str
+    
+#     description:str
+#     price:int
+#     image:str
+        
