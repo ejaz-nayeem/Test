e5e9e7a88d88e085b2d0430baff2a4839a981e3c
ejaz-nayeem <muhammadejazbinnayeem@gmail.com>
2025-07-21 20:19:59 +0600

    update

--- travello/models.py
+++ travello/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id:int
-    destination_name:str
+class Destination(models.Model):
     
-    description:str
-    price:int
-    image:str
-    offer:bool
+    destination_name=models.CharField(max_length=100)
+    
+    description=models.TextField()
+    price=models.IntegerField(default=0)
+    image=models.ImageField(upload_to='pics')
+    offer=models.BooleanField(default=False)
 
 # class Destination2:
 #     id:int
--- travello/views.py
+++ travello/views.py
@@ -5,27 +5,7 @@ from .models import Destination
 # Create your views here.
 def index(request):
     
-    desc1=Destination()
-    desc1.name='dhaka'
-    desc1.description='dhakadddddddddd'
-    desc1.price=1
-    desc1.image='destination_1.jpg'
-    desc1.offer=False 
+    descs=Destination.objects.all()
     
-    desc2=Destination()
-    desc2.name='CTG'
-    desc2.description='ctggggggg'
-    desc2.price=2
-    desc2.image='destination_2.jpg'
-    desc2.offer=True
-    
-    desc3=Destination()
-    desc3.name='Mymen'
-    desc3.description='myyymmmmmm'
-    desc3.price=3
-    desc3.image='destination_3.jpg'
-    desc3.offer=False
-    
-    descs=[desc1, desc2, desc3]
     
     return render(request, 'index.html', {'descs':descs})
